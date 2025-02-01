bc6e9fb6ba64467825bb4f28b2511d09ebe283cf
lgrin-byte <lgrin02@naver.com>
2025-02-02 02:05:46 +0900

    feat: UI작업

---
+++ src/component/TeamCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function TeamCard() {
+  return (
+    <li className="teamInfo-card">
+      <h4 className="teamInfo-type">동그리 마을</h4>
+      <div className="teamInfo-members">
+        <span>박연지</span>, 엄다빈, 명복음, 명노엘, 박민규, 강다운, 박수연
+      </div>
+    </li>
+  );
+}
