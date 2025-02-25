28d97bc2f886ffd13d07607ecc9c9ffc0f1bc803
novel <shduf123@naver.com>
2025-02-25 20:07:25 +0900

    feat: 마을 데이터 연동

--- src/component/TeamInfo.jsx
+++ src/component/TeamInfo.jsx
@@ -1,7 +1,28 @@
-import React, { useEffect } from "react";
-import TeamCard from "./TeamCard";
+import React, { useState, useEffect } from "react";
+import {db} from "../firbase"
+import { collection, getDocs } from "firebase/firestore"; 
 
 function TeamInfo() {
+
+    const [teamInfoData, setteamInfoData] = useState([]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const querySnapshot = await getDocs(collection(db, "team"));
+          const data = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setteamInfoData(data);
+        } catch (error) {
+          console.error("Error fetching data: ", error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
   return (
     <div className="teamInfo-container">
       <div class="teamInfo-header">
@@ -12,7 +33,14 @@ function TeamInfo() {
         </p>
       </div>
       <ul>
-        <TeamCard />
+      {teamInfoData.map((item) => (
+         <li key={item.id} className="teamInfo-card">
+              <h4 className="teamInfo-type">{item.villageName}</h4>
+              <div className="teamInfo-members">
+                <span>{item.chief}</span>,{item.members}
+              </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
