98846d11738d9cfdeacd720b63a7f558de840dec
novel <shduf123@naver.com>
2025-02-25 20:19:43 +0900

    feat: 광고 데이터 연동

--- src/component/Ad.jsx
+++ src/component/Ad.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {db} from "../firbase"
+import { collection, getDocs } from "firebase/firestore"; 
 
 export default function Ad() {
+
+    const [adData, setAdData] = useState([]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const querySnapshot = await getDocs(collection(db, "ads"));
+          const data = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setAdData(data);
+        } catch (error) {
+          console.error("Error fetching data: ", error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
   return (
     <div class="notice-container">
       <div class="notice-header">
@@ -10,35 +32,13 @@ export default function Ad() {
         <h1>광고</h1>
       </div>
 
-      <div class="notice-section">
-        <h2>1. 이번주 2부 순서</h2>
-        <p>생일파티 및 말씀 나눔 입니다.</p>
-      </div>
-
-      <div class="notice-section">
-        <h2>
-          2. 예배를 위한 기도모임<span class="prayer-emoji">🙏</span>
-        </h2>
-        <p>예배 전 오후 1시 40분에 성전에서 기도모임이 있습니다!</p>
-        <p>예배를 사모하는 마음으로 모두 함께 모여 기도하면 좋겠습니다.</p>
-      </div>
-
-      <div class="notice-section">
-        <h2>3. 연말정산 (기부금 영수증 신청)</h2>
-        <p>연말 정산, 기부금 영수증이 필요하신 분들은 2층 교회 사무실로 신청해주시기 바랍니다.</p>
-        
-      </div>
-
-      <div class="notice-section">
-        <h2>4. 이소리 청년 결혼</h2>
-        <p>일시 : 1월 18일(토) 오전 11시</p>
-        <p>장소 : ~~~~~~2층 (수원) / 어쩌구어쩌구 노벨 존잘</p>
-      </div>
-
-      <div class="notice-section">
-        <h2>5. 다음 주 2부 순서</h2>
-        <div class="member-list">알베스 개하남자 리니 개하여자 알베스 개하남자 리니 개하여자</div>
-      </div>
+      {adData.map((item) => (
+         
+          <div key={item.id} className="notice-section">
+            <h2>{item.title}</h2>
+            <p>{item.content}</p>
+          </div>
+        ))}
     </div>
   );
 }
