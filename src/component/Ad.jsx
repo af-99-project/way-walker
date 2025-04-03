e30e841abff1c6897b8c4f3550698e3512b6b08d
novel <novel@sooplive.com>
2025-04-03 19:17:16 +0900

    feat: 섬김이 데이터연동 및 calss -> calssName으로 수정

--- src/component/Ad.jsx
+++ src/component/Ad.jsx
@@ -1,44 +1,42 @@
 import React, { useState, useEffect } from "react";
-import {db} from "../firbase"
-import { collection, getDocs } from "firebase/firestore"; 
+import { db } from "../firbase";
+import { collection, getDocs } from "firebase/firestore";
 
-export default function Ad({elementRef}) {
+export default function Ad({ elementRef }) {
+  const [adData, setAdData] = useState([]);
 
-    const [adData, setAdData] = useState([]);
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const querySnapshot = await getDocs(collection(db, "ads"));
-          const data = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
-          setAdData(data);
-        } catch (error) {
-          console.error("Error fetching data: ", error);
-        }
-      };
-  
-      fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "ads"));
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setAdData(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div class="notice-container" ref={elementRef}>
-      <div class="notice-header">
-        <span class="megaphone-icon">
+    <div className="notice-container" ref={elementRef}>
+      <div className="notice-header">
+        <span className="megaphone-icon">
           <img src="/src/assets/loudspeaker.svg" alt="mainLogo" />
         </span>
         <h1>광고</h1>
       </div>
 
       {adData.map((item) => (
-         
-          <div key={item.id} className="notice-section">
-            <h2>{item.title}</h2>
-            <p>{item.content}</p>
-          </div>
-        ))}
+        <div key={item.id} className="notice-section">
+          <h2>{item.title}</h2>
+          <p>{item.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
