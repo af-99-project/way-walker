e44bd3fd60fc119c67d6206bf5bd6774ef5b58d2
novel <novel@sooplive.com>
2025-05-12 01:51:31 +0900

    feat: capslock 확인 기능 추가

--- src/component/admin/LoginPage.jsx
+++ src/component/admin/LoginPage.jsx
@@ -5,36 +5,48 @@ import { useNavigate } from "react-router-dom";
 const LoginPage = () => {
   const [inputPassword, setInputPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const dummyPassword = "1234";
+  const dummyPassword = "1234"; // 임시 비밀번호
   const [error, setError] = useState("");
+  const [capsLock, setCapsLock] = useState("");
   const { setIsLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (inputPassword === dummyPassword) {
-      setIsLoggedIn(true); // ✅ 로그인 상태 true로
-      setError("");
-      navigate("/admin"); // ✅ 관리자 페이지로 이동
+      setIsLoggedIn(true); // 로그인 상태 true로
+      navigate("/admin"); // 관리자 페이지로 이동
     } else {
       setError("비밀번호가 틀렸습니다.");
     }
   };
 
+  function onKeyDownHandler(e) {
+    let isCapsLock = e.getModifierState("CapsLock");
+    setCapsLock(isCapsLock);
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type={showPassword ? "text" : "password"}
-        placeholder="비밀번호"
-        value={inputPassword}
-        onChange={(e) => setInputPassword(e.target.value)}
-      />
-      <button type="button" onClick={() => setShowPassword(!showPassword)}>
-        {showPassword ? "숨기기" : "보이기"}
-      </button>
-      <button type="submit">로그인</button>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-    </form>
+    <div className="adminPassWord">
+      <form onSubmit={handleSubmit}>
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="비밀번호"
+          value={inputPassword}
+          onChange={(e) => setInputPassword(e.target.value)}
+          onKeyDown={onKeyDownHandler}
+          onKeyUp={onKeyDownHandler}
+        />
+        <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword)}
+          className={`pwIcon ${showPassword ? "show" : "hide"}`}
+        ></button>
+        <button type="submit">로그인</button>
+        {error ? <p style={{ color: "red" }}>{error}</p> : <p></p>}
+        {capsLock ? <p>CapsLock이 켜져있습니다.</p> : <p></p>}
+      </form>
+    </div>
   );
 };
 
