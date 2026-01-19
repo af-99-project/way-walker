5f2e9bfc0886eb288f543cb35115ce78c58382cb
novel <novel@sooplive.com>
2026-01-19 19:04:51 +0900

    feat: 상단 스크롤 시 애니메이션 적용

--- src/component/common/Header.jsx
+++ src/component/common/Header.jsx
@@ -1,8 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Header() {
+  const [showBg, setShowBg] = useState(false);
+
+  useEffect(() => {
+    const handleShowBg = () => {
+      if (window.scrollY > 100) {
+        setShowBg(true);
+      } else {
+        setShowBg(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleShowBg);
+    return () => {
+      window.removeEventListener("scroll", handleShowBg);
+    };
+  }, []);
+
   return (
-    <header id="header">
+    <header id="header" className={`${showBg ? "active" : ""}`}>
       <h1 className="logo">
         <a href="#">청년여호수아</a>
       </h1>
