9db426ea3acd3a52e1b7069a8f0f5598a56c8895
novel <shduf123@naver.com>
2026-02-07 13:48:56 +0900

    feat: 바텀네비 추가

--- src/components/common/Main.tsx
+++ src/components/common/Main.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Navigation } from "../Navigation";
 import { SermonSection } from "../SermonSection";
 import { WorshipOrder } from "../WorshipOrder";
@@ -13,27 +13,44 @@ import { ContactSection } from "../ContactSection";
 import Header from "./Header";
 import FixBtn from "./FixBtn";
 import TopVisual from "../TopVisual";
+import BottomNav from "./BottomNav";
+
+
 
 export default function Main() {
   const [activeSection, setActiveSection] = useState("home");
 
+  
+  
+  const elementRef = useRef(null);
+
+  const sectionRefs = {
+      worship: useRef<HTMLElement | null>(null), // 예배 순서
+      calendar: useRef<HTMLElement | null>(null), // 캘린더
+      team: useRef<HTMLElement | null>(null), // 마을 소개
+      ad: useRef<HTMLElement | null>(null), // 광고
+  };
+
+  type Props = {
+    elementRef?: React.RefObject<HTMLElement>;
+  };
   return (
     <div className="min-h-screen bg-white">
-      {/* <Navigation activeSection={activeSection} setActiveSection={setActiveSection} /> */}
       <Header />
       <main>
-        <TopVisual />
-        <WorshipOrder />
-        <WeeklySchedule />
-        <LiveStream />
+        <TopVisual /> {/* 상단 비쥬얼 영역 */}
+        <WorshipOrder elementRef={sectionRefs.worship} /> {/* 예배순서 */}
+        <WeeklySchedule elementRef={sectionRefs.calendar} />{/* 캘린더 */}
+        <LiveStream />{/* 라이브 */}
         {/* <PrayerRequest /> 기도요청 */}
-        <VillageIntro />
+        <VillageIntro elementRef={sectionRefs.team} /> {/*  */}
         <OnlineGiving />
-        <Announcements />
+        <Announcements elementRef={sectionRefs.ad} />
         <ContactSection />
       </main>
       <FixBtn />
       <Footer />
+      <BottomNav sectionRefs={sectionRefs} />
     </div>
   );
 }
